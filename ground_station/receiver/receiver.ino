785773196142fd01e8a47f5ff4114a48a377926d
vebovs <vebovs@hotmail.no>
2024-03-29 13:58:58 +0100

    receiver bit interpretation

--- ground_station/receiver/receiver.ino
+++ ground_station/receiver/receiver.ino
@@ -5,7 +5,7 @@ const uint8_t CS_PIN = 24;
 const uint8_t INT_PIN = 25;
 
 const float rx_freq = 433.600; // in Mhz
-const long sbw = 20800; // 20.8 kHz
+const long sbw = 62500; // in kHz
 const uint8_t sf = 8;
 const uint8_t crc = 7; //denominator, final value is 4/7
 
@@ -31,9 +31,9 @@ void loop() {
   if (rf95.waitAvailableTimeout(3000)) {
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len)) {
-      RH_RF95::printBuffer("request: ", buf, len);
-      Serial.print("got request: ");
-      Serial.println((char*)buf);
+      Serial.println("Received data: ");
+      printBytes(buf, 24);
+      Serial.println();
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
     }
@@ -43,6 +43,20 @@ void loop() {
   }
   else{
     Serial.println("Nothing received...");
+  }
+}
+
+void printBytes(uint8_t* bytes, uint8_t len) {
+  for(uint8_t i = 0; i < len; i++) {
+    printBitsFromByte(bytes[i]);
+  }
+}
+
+void printBitsFromByte(uint8_t val) {
+  for(uint8_t i = 0; i < 8; i++) {
+    bool b = val & 0x80;
+    Serial.print(b);
+    val = val << 1;
   }
 }
 
