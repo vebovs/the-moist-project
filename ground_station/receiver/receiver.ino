710f89d0bce86a2fe96105fbaa2227193fb6b9c9
vebovs <vebovs@hotmail.no>
2024-03-31 13:02:09 +0200

    interpreting bit string as sensor data

--- ground_station/receiver/receiver.ino
+++ ground_station/receiver/receiver.ino
@@ -13,6 +13,14 @@ RH_RF95 rf95(CS_PIN, INT_PIN);
 
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
+const uint8_t data_budget_size = 24; // bytes to be decoded
+
+const uint16_t bits_pressure = 64;
+const uint16_t bits_reading = bits_pressure + 32;
+const uint16_t bits_rh = bits_reading + 32;
+const uint16_t bits_co2 = bits_rh + 32;
+const uint16_t bits_temp = bits_co2 + 32;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial); // Wait for serial port to be available
@@ -32,8 +40,21 @@ void loop() {
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len)) {
       Serial.println("Received data: ");
-      printBytes(buf, 24);
-      Serial.println();
+      char bit_str_buf[data_budget_size * 8];
+      bytesToBitStr(buf, data_budget_size, bit_str_buf);
+      String bit_str = String(bit_str_buf);
+
+      Serial.println(bit_str_buf);
+      Serial.print(bitStringToFloat(bit_str.substring(bits_pressure, bits_reading).c_str()));
+      Serial.print(", ");
+      Serial.print(bitStringToDouble(bit_str.substring(0, bits_pressure).c_str()));
+      Serial.print(", ");
+      Serial.print(bitStringToFloat(bit_str.substring(bits_co2, bits_temp).c_str()));
+      Serial.print(", ");
+      Serial.print(bitStringToFloat(bit_str.substring(bits_reading, bits_rh).c_str()));
+      Serial.print(", ");
+      Serial.println(bitStringToFloat(bit_str.substring(bits_rh, bits_co2).c_str()));
+
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
     }
@@ -46,16 +67,36 @@ void loop() {
   }
 }
 
-void printBytes(uint8_t* bytes, uint8_t len) {
+float bitStringToFloat(const char* str) {
+  uint32_t x = 0;
+  for(; *str; ++str) {
+    x = (x << 1) + (*str - '0');
+  }
+  float f;
+  memcpy(&f, &x, 4); // 4 because float is 32 bits
+  return f;
+}
+
+double bitStringToDouble(const char* str) {
+  uint64_t x = 0;
+  for(; *str; ++str) {
+    x = (x << 1) + (*str - '0');
+  }
+  double d;
+  memcpy(&d, &x, 8); // 8 because double is 64 bits
+  return d;
+}
+
+void bytesToBitStr(uint8_t* bytes, uint8_t len, char* str) {
   for(uint8_t i = 0; i < len; i++) {
-    printBitsFromByte(bytes[i]);
+    byteToBits(bytes[i], str, i);
   }
 }
 
-void printBitsFromByte(uint8_t val) {
+void byteToBits(uint8_t val, char* str, uint8_t len) {
   for(uint8_t i = 0; i < 8; i++) {
     bool b = val & 0x80;
-    Serial.print(b);
+    str[len * 8 + i] = b == 1 ? '1' : '0';
     val = val << 1;
   }
 }
