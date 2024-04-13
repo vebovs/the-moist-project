e54846ab98abf220fe134c2abea0310fe3a599fe
unknown <vebovs@hotmail.no>
2024-04-13 21:59:27 +0200

    debug mode for ground station

--- ground_station/receiver/receiver.ino
+++ ground_station/receiver/receiver.ino
@@ -4,6 +4,9 @@
 #include <float16.h>
 #include <moist.h>
 
+// Set to 0 while capturing data
+#define DEBUG 1
+
 RH_RF95 rf95(CS_PIN, INT_PIN);
 
 uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
@@ -22,11 +25,15 @@ const uint16_t bits_co2 = bits_rh + 32;
 const uint16_t bits_temp = bits_co2 + 16;
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) delay(10); // Wait for serial port to be available
+  #if DEBUG
+    Serial.begin(9600);
+    while (!Serial) delay(10); // Wait for serial port to be available
+  #endif
 
   if (!rf95.init()) {
-    Serial.println("Radio initialisation failed...");
+    #if DEBUG
+      Serial.println("Radio initialisation failed...");
+    #endif
   }
 
   rf95.setFrequency(FREQUENCY);
@@ -39,52 +46,89 @@ void loop() {
   if (rf95.waitAvailableTimeout(3000)) {
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len)) {
-      //Serial.println("Received data: ");
       char bit_str_buf[DATA_BUDGET_SIZE * 8];
       bytesToBitStr(buf, DATA_BUDGET_SIZE, bit_str_buf);
       String bit_str = String(bit_str_buf);
 
-      //Elapsed time since start
-      Serial.print(hour());
-      Serial.print(":");
-      Serial.print(minute());
-      Serial.print(":");
-      Serial.print(second());
-      Serial.print(",");
+      #if DEBUG
+        Serial.print("Elapsed time: ");
+        Serial.print(hour());
+        Serial.print(":");
+        Serial.print(minute());
+        Serial.print(":");
+        Serial.println(second());
+        Serial.print("ID: ");
+        Serial.println(bitStringToInt32(bit_str.substring(0, bits_id).c_str()));
+        Serial.print("Timestamp: ");
+        Serial.print(bitStringToUint8(bit_str.substring(bits_id, bits_hour).c_str()));
+        Serial.print(":");
+        Serial.print(bitStringToUint8(bit_str.substring(bits_hour, bits_minute).c_str()));
+        Serial.print(":");
+        Serial.println(bitStringToUint8(bit_str.substring(bits_minute, bits_second).c_str()));
+        Serial.print("Altitude: ");
+        Serial.println(bitStringToFloat(bit_str.substring(bits_second, bits_altitude).c_str()));
+        Serial.print("Latitude: ");
+        Serial.println(bitStringToFloat(bit_str.substring(bits_altitude, bits_lat).c_str()));
+        Serial.print("Longitude: ");
+        Serial.println(bitStringToFloat(bit_str.substring(bits_lat, bits_lng).c_str()));
+        Serial.print("Pressure: ");
+        Serial.println(bitStringToFloat(bit_str.substring(bits_lng, bits_pressure).c_str()));
+        Serial.print("NTC: ");
+        Serial.println(bitStringToFloat(bit_str.substring(bits_pressure, bits_reading).c_str()));
+        Serial.print("Relative humidity: ");
+        Serial.println(bitStringToFloat16(bit_str.substring(bits_reading, bits_rh).c_str()));
+        Serial.print("CO2: ");
+        Serial.println(bitStringToFloat(bit_str.substring(bits_rh, bits_co2).c_str()));
+        Serial.print("Temperature: ");
+        Serial.println(bitStringToFloat16(bit_str.substring(bits_co2, bits_temp).c_str()));
+        Serial.print("RSSI: ");
+        Serial.println(rf95.lastRssi(), DEC);
+        Serial.println();
+      #endif
 
-      //Serial.println(bit_str_buf);
-      Serial.print(bitStringToInt32(bit_str.substring(0, bits_id).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToUint8(bit_str.substring(bits_id, bits_hour).c_str()));
-      Serial.print(":");
-      Serial.print(bitStringToUint8(bit_str.substring(bits_hour, bits_minute).c_str()));
-      Serial.print(":");
-      Serial.print(bitStringToUint8(bit_str.substring(bits_minute, bits_second).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat(bit_str.substring(bits_second, bits_altitude).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat(bit_str.substring(bits_altitude, bits_lat).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat(bit_str.substring(bits_lat, bits_lng).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat(bit_str.substring(bits_lng, bits_pressure).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat(bit_str.substring(bits_pressure, bits_reading).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat16(bit_str.substring(bits_reading, bits_rh).c_str()));
-      Serial.print(",");
-      Serial.print(bitStringToFloat(bit_str.substring(bits_rh, bits_co2).c_str()));
-      Serial.print(",");
-      Serial.println(bitStringToFloat16(bit_str.substring(bits_co2, bits_temp).c_str()));
+      #if !DEBUG
+        //Elapsed time since start
+        Serial.print(hour());
+        Serial.print(":");
+        Serial.print(minute());
+        Serial.print(":");
+        Serial.print(second());
+        Serial.print(",");
+        Serial.print(bitStringToInt32(bit_str.substring(0, bits_id).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToUint8(bit_str.substring(bits_id, bits_hour).c_str()));
+        Serial.print(":");
+        Serial.print(bitStringToUint8(bit_str.substring(bits_hour, bits_minute).c_str()));
+        Serial.print(":");
+        Serial.print(bitStringToUint8(bit_str.substring(bits_minute, bits_second).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat(bit_str.substring(bits_second, bits_altitude).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat(bit_str.substring(bits_altitude, bits_lat).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat(bit_str.substring(bits_lat, bits_lng).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat(bit_str.substring(bits_lng, bits_pressure).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat(bit_str.substring(bits_pressure, bits_reading).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat16(bit_str.substring(bits_reading, bits_rh).c_str()));
+        Serial.print(",");
+        Serial.print(bitStringToFloat(bit_str.substring(bits_rh, bits_co2).c_str()));
+        Serial.print(",");
+        Serial.println(bitStringToFloat16(bit_str.substring(bits_co2, bits_temp).c_str()));
+      #endif
       
-      //Serial.print("RSSI: ");
-      //Serial.println(rf95.lastRssi(), DEC);
     }
     else {
-      Serial.println("recv failed");
+      #if DEBUG
+        Serial.println("recv failed");
+      #endif
     }
   }
-  else{
-    //Serial.println("Nothing received...");
+  else {
+    #if DEBUG
+      Serial.println("Nothing received...");
+    #endif
   }
 }
