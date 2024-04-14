90fa5ad08dc713c399b5757a5c44cbd83b33c882
unknown <vebovs@hotmail.no>
2024-04-14 13:50:07 +0200

    only install gy91 through zip install

--- ci/build-teensy.sh
+++ ci/build-teensy.sh
@@ -22,9 +22,12 @@ arduino-cli core update-index --additional-urls https://www.pjrc.com/teensy/pack
 # Install Teensy AVR core for Teensy 4.1
 arduino-cli core install teensy:avr
 
+# Link Arduino library
+ln -s $GITHUB_WORKSPACE $HOME/Arduino/libraries/moist
+
 # Install dependencies
 arduino-cli lib install float16@0.2.0
-arduino-cli lib install --zip-path ./dependencies/*.zip --config-file "./ci/config.yml"
+arduino-cli lib install --zip-path ./dependencies/GY91.zip --config-file "./ci/config.yml"
 
 # Compile ino files in the ground station subfolder
 for file in ./ground_station/**/*.ino ; do
