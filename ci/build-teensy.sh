55ccca4c91751c8c5b62b2cc30e4a6e247abb29a
unknown <vebovs@hotmail.no>
2024-04-16 20:54:06 +0200

    added auint install in build

--- ci/build-teensy.sh
+++ ci/build-teensy.sh
@@ -25,6 +25,7 @@ arduino-cli core install teensy:avr
 arduino-cli lib install float16@0.2.0
 arduino-cli lib install "Adafruit SCD30"@1.0.11
 arduino-cli lib install TinyGPSPlus@1.0.3
+arduino-cli lib install AUnit@1.7.1
 arduino-cli lib install --zip-path ./dependencies/*.zip --config-file "./ci/config.yml"
 
 # Compile ino files in the ground station subfolder
